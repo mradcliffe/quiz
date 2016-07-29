58614e1028ab3b741fffcbda32f14fce2adb33de
Matthew Radcliffe <mradcliffe@softpixel.com>
2016-07-28 21:32:33 -0400

    Issue #1 by mradcliffe: Add a base info file, module file and test to get quiz running on Drupal 8

---
+++ src/Tests/QuizTestBase.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\quiz\Tests;
+
+
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Base test class for Quiz functional tests.
+ *
+ * @group Quiz
+ *
+ * @todo Change to abstract class.
+ */
+class QuizTestBase extends WebTestBase {
+
+  static protected $modules = ['views', 'quiz'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   * A simple test to assert that the module was installed.
+   */
+  public function testModuleInstalled() {
+    /** @var \Drupal\Core\Extension\ModuleHandlerInterface $module_handler */
+    $module_handler = \Drupal::service('module_handler');
+
+    $this->assertTrue($module_handler->moduleExists('quiz'));
+  }
+
+}
