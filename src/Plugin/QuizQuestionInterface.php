cfeb8ae08a1df17bd20bbf7c47d2bae228a22c18
Matthew Radcliffe <mradcliffe@softpixel.com>
2016-08-06 09:57:47 -0400

    Issue #4 by mradcliffe: Remove methods on QuizQuestionInterface until it is further thought out

--- src/Plugin/QuizQuestionInterface.php
+++ src/Plugin/QuizQuestionInterface.php
@@ -5,22 +5,4 @@ namespace Drupal\quiz\Plugin;
 /**
  * Quiz question plugin interface.
  */
-interface QuizQuestionInterface {
-
-  /**
-   * Get field definitions for the question type.
-   *
-   * @return array
-   *   An array of field definitions keyed by the field name.
-   */
-  public function getQuestionFieldDefinitions();
-
-  /**
-   * Get field definitions for the question response type.
-   *
-   * @return array
-   *   An array of field definitions keyed by the field name.
-   */
-  public function getQuestionResponseFieldDefinitions();
-
-}
+interface QuizQuestionInterface {}
